316dfe28e732a8f6ebc05d9c3e1e5202ce5e5c8b
ouibene <ouibene@gmail.com>
2020-09-09 23:20:41 +0900

    Add 42576

---
+++ programmers/q42576.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+	int i = 0;
+        Arrays.sort(participant);
+	Arrays.sort(completion);
+		
+	for (i = 0; i < completion.length; i++) {
+	    if (participant[i].equals(completion[i]) == false) 
+                return participant[i];
+	}
+        return participant[i];
+    }
+}
