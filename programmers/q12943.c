fdb485a174a2d5dac78ca6e6637400ac882b0c3c
youngeun_lee <youngeun_lee2@tmax.co.kr>
2020-09-17 22:17:32 +0900

    q12943.c

---
+++ programmers/q12943.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(long num) {
+    int cnt = 0;
+    
+    if (num == 1) return 0;
+    while (num != 1 && cnt < 500) {
+        if (num % 2 == 0) num /= 2;
+        else num = (num * 3) + 1;
+        
+        cnt++;    
+        if (num == 1) return cnt;
+    }
+    
+    return -1;
+}
